c0b42bce80e2ddd15078d321029eb882922618c1
Richard Anaya <richard.anaya@gmail.com>
2021-04-26 20:47:02 -0700

    adding cors

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::thread;
 use std::time;
 use tide::prelude::*;
 use tide::{Request, Response};
+use http_types::headers::HeaderValue;
+use tide::security::{CorsMiddleware, Origin};
+
 use webbrowser;
 
 #[derive(Default)]
@@ -73,6 +76,12 @@ fn main() {
 
     task::block_on(async {
         let mut app = tide::new();
+
+        let cors = CorsMiddleware::new()
+            .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+            .allow_origin(Origin::from("*"))
+            .allow_credentials(false);
+        app.with(cors);
         app.at("/execute").post(move |mut req: Request<()>| {
             let f = task::block_on(async { req.body_string().await.unwrap() });
             let mut a = globals::get::<AllResults>();
